10bc662806ee584bd3b2c7e3826d771a38cf08a6
Tai Truong <giaptai170@gmail.com>
2019-12-25 11:58:37 +0700

    Gộp thành 1 file Thêm vòng lặp và va chạm

--- game.py
+++ game.py
@@ -1,74 +1,91 @@
-import sys
-import pygame
+import math
 import random
-from face import face
-from enemy import Enemy
+import pygame
 
-class Game:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((500, 700))
-        self.bg_color = (100, 100, 100)
-        self.face = face(self)
-        self.enemies = pygame.sprite.Group()
-        self.create_enemy_group()    
-       
-    def run_game(self):
-        while True:
-            self.check_event()
-            self.face.update()
-            self.enemies.update()
-            self.update_game()
+pygame.init()
+screen = pygame.display.set_mode((400, 700))
+background = pygame.image.load('images/background.png')
+pygame.display.set_caption("GT")
+
+
+
+# Player
+playerImg = pygame.image.load('images/player.png')
+playerX = 180
+playerY = 580
+playerX_change = 0
+
+# Enemy
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyY_change = 1
+num_of_enemies = 1
+
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('images/enemy.png'))
+    enemyX.append(random.randint(0, 350))
+    enemyY.append(random.randint(50, 150))
+
+
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
+
+def isCollision(enemyX, enemyY, playerX, playerY):
+    distance = math.sqrt(math.pow(enemyX - playerX, 2) + (math.pow(enemyY - playerY, 2)))
+    if distance < 40:
+        return True
+    else:
+        return False
+
+
+
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 3
            
-            
 
-    def check_event(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            # Check key pressed
-            elif event.type == pygame.KEYDOWN:
-                self.check_keydown(event)
-            # Check key released
-            elif event.type == pygame.KEYUP:
-                self.check_keyup(event)
-                    
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
 
-    def update_game(self):
-        self.screen.fill(self.bg_color)
-        self.face.blitme()
-        self.enemies.draw(self.screen)
-        pygame.display.flip()
+   #Giới hạn di chuyển 
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 350:
+        playerX = 350
 
-    def check_keydown(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.face.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.face.moving_left = True
-        
-    def check_keyup(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.face.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.face.moving_left = False
+    # Enemy Movement
+    for i in range(num_of_enemies):
+        enemyY[i] += enemyY_change
+
+
+        # Collision
+        collision = isCollision(enemyX[i], enemyY[i], playerX, playerY)
+        if collision == True:
+            enemyX[i] = random.randint(0, 350)
+            enemyY[i] = random.randint(50, 150)
+			
+        enemy(enemyX[i], enemyY[i], i)
+		
+		
     
-    def create_enemy_group(self):
-        number = 2
-        self.create_enemies(number)
-        
-    def create_enemies(self, number):
-        for order in range(number):
-            enemy = Enemy(self)
-            enemy.rect.x = random.randrange(0,400)
-            self.enemies.add(enemy)
-            
-    
-
-            
-
-
-        
-if __name__ == '__main__':
-    game = Game()
-    game.run_game()
-    
+    player(playerX, playerY)
+    pygame.display.update()
